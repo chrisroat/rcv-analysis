9bc8b8c50108260542be2a1750cbfa8f01686869
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2023-09-17 12:07:02 -0700

    analysis updates

--- analysis.py
+++ analysis.py
@@ -1,17 +1,17 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from matplotlib.colors import ListedColormap
 from matplotlib.ticker import MaxNLocator
 from scipy.cluster import hierarchy
 
 
 def go(df):
     for _, df_contest in df.groupby(["contest_id"]):
-        fig_corr = plot_corr(df_contest)
-        fig_corr.show()
-        fig_results, df_results = get_results(df_contest)
-        fig_results.show()
-        print(df_results)
+        names_ordered = plot_corr(df_contest)
+        plot_results(df_contest)
+        plot_combos(df_contest, names_ordered)
 
 
 def plot_corr(df):
@@ -25,11 +25,12 @@ def plot_corr(df):
         fill_value=0,
     )
     corr = pivot_table.corr()
-    plot(corr, axes[0])
+    corr_names = plot(corr, axes[0], symmetric=True)
     frac = pivot_table.T.dot(pivot_table) / pivot_table.sum()
-    plot(frac, axes[1], is_frac=True)
+    plot(frac, axes[1], symmetric=False)
     fig.tight_layout()
-    return fig
+
+    return corr_names
 
 
 def get_title(df):
@@ -45,17 +46,18 @@ def get_title(df):
     return title
 
 
-def plot(matrix, ax, is_frac=False):
+def plot(matrix, ax, symmetric):
     matrix = cluster_order(matrix)
     size = matrix.shape[0]
     mask = np.triu(np.repeat(True, size))
     limit = np.abs(matrix.values[~mask]).max()
-    if is_frac:
-        cmap = "Reds"
-        vmin = 0
-    else:
-        cmap = "vlag"
+    cmap = matplotlib.cm.get_cmap("vlag")
+    if symmetric:
         vmin = -limit
+    else:
+        mid = len(cmap.colors) // 2
+        cmap = ListedColormap(cmap.colors[mid:])
+        vmin = 0
     sns.heatmap(
         matrix,
         square=True,
@@ -65,14 +67,15 @@ def plot(matrix, ax, is_frac=False):
         ax=ax,
         cbar_kws={"shrink": 0.8},
     )
-    if is_frac:
-        ax.set_title("Overlapping Fraction")
-        ax.set_xlabel("total")
-        ax.set_ylabel("with")
-    else:
+    if symmetric:
         ax.set_title("Mutual Correlation")
         ax.set_xlabel("")
         ax.set_ylabel("")
+    else:
+        ax.set_title("Overlapping Fraction")
+        ax.set_xlabel("total")
+        ax.set_ylabel("with")
+    return matrix.index.tolist()
 
 
 def cluster_order(df):
@@ -83,7 +86,7 @@ def cluster_order(df):
     return df.iloc[idx, idx]
 
 
-def get_results(df):
+def plot_results(df):
     vote_for = df["vote_for"].unique()
     assert len(vote_for) == 1
     vote_for = vote_for[0]
@@ -100,9 +103,13 @@ def get_results(df):
     df_stats = df_stats.groupby("cvr_id")["is_winner"]
     df_stats = df_stats.agg(num_marks="count", num_winners="sum")
 
-    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(6, 18))
-    sns.histplot(df_stats, x="num_winners", discrete=True, stat="percent", ax=axes[0])
-    sns.histplot(df_stats, x="num_marks", discrete=True, stat="percent", ax=axes[1])
+    fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(6, 24))
+    sns.histplot(
+        df_votes, x="name", weights="votes", hue="result", stat="percent", ax=axes[0]
+    )
+    axes[0].tick_params(axis="x", rotation=90)
+    sns.histplot(df_stats, x="num_winners", discrete=True, stat="percent", ax=axes[1])
+    sns.histplot(df_stats, x="num_marks", discrete=True, stat="percent", ax=axes[2])
     sns.histplot(
         df_stats,
         x="num_marks",
@@ -110,13 +117,47 @@ def get_results(df):
         multiple="dodge",
         discrete=True,
         stat="percent",
-        ax=axes[2],
+        ax=axes[3],
     )
 
-    axes[0].xaxis.set_major_locator(MaxNLocator(integer=True))
     axes[1].xaxis.set_major_locator(MaxNLocator(integer=True))
     axes[2].xaxis.set_major_locator(MaxNLocator(integer=True))
+    axes[3].xaxis.set_major_locator(MaxNLocator(integer=True))
 
     fig.tight_layout()
 
-    return fig, df_votes
+
+def plot_combos(df, names):
+    df_comb = df.pivot_table(
+        columns="name", index="cvr_id", aggfunc="size", fill_value=0
+    )
+    df_comb = df_comb.groupby(names).size()
+    df_comb = df_comb.reset_index(name="Count").sort_values("Count", ascending=False)
+
+    yticks = []
+    ytick_labels = []
+    borders = [0]
+    data = []
+    prev = 0
+    for i in range(df_comb.shape[0]):
+        values = df_comb.iloc[i].tolist()
+        *values, count = values
+        yticks.append(prev + count // 2)
+        ytick_labels.append(count)
+        borders.append(borders[-1] + count)
+        prev += count
+        data.extend([values] * count)
+
+    fig, ax = plt.subplots(figsize=(8, 20))
+    ax = sns.heatmap(data, cmap="Blues", ax=ax, cbar=False, xticklabels=names)
+    ax.xaxis.tick_top()
+    ax.set_ylabel("Ballot Count for Combination")
+    plt.xticks(rotation=90)
+    plt.yticks(ticks=yticks, labels=ytick_labels, fontsize=6)
+
+    for border in borders:
+        ax.axhline(border, color="white", lw=0.2)
+
+    for x in range(len(names)):
+        ax.axvline(x, color="white", lw=2)
+    fig.tight_layout()
